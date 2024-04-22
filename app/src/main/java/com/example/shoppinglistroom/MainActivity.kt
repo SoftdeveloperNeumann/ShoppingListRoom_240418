e7c787bd3d626190c86d8e7d973705ad393eca48
Frank <f.neumann.berlin@googlemail.com>
2024-04-22 09:25:22 +0200

    mit Delete und SnackBar

--- app/src/main/java/com/example/shoppinglistroom/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistroom/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.shoppinglistroom
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoppinglistroom.adapter.ShoppingMemoListAdapter
 import com.example.shoppinglistroom.database.ShoppingMemo
 import com.example.shoppinglistroom.databinding.ActivityMainBinding
 import com.example.shoppinglistroom.viewmodel.ShoppingMemoViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var shoppinMemoViewModel: ShoppingMemoViewModel
+    private lateinit var itemTouchHelper: ItemTouchHelper
     private val adapter = ShoppingMemoListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,6 +67,42 @@ class MainActivity : AppCompatActivity() {
         binding.etProduct.setOnEditorActionListener { _, _, _ ->
             binding.btnAddProduct.performClick()
         }
+
+        itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
+            0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val currentMemo = shoppinMemoViewModel.getAllShoppingMemos()?.value?.get(viewHolder.adapterPosition)
+
+                when(direction){
+                    ItemTouchHelper.RIGHT ->{ // Eintrag löschen
+                        shoppinMemoViewModel.delete(currentMemo!!)
+                        Snackbar.make(
+                            this@MainActivity,
+                            binding.root,
+                            "Löschen rückgängig machen",
+                            Snackbar.LENGTH_LONG)
+                            .setAction(
+                                "UNDO"
+                            ) { shoppinMemoViewModel.insertOrUpdate(currentMemo) }.show()
+
+                    }
+                   ItemTouchHelper.LEFT ->{ // Eintrag editieren
+
+                   }
+                }
+            }
+        })
+
+        itemTouchHelper.attachToRecyclerView(binding.rvShoppingMemos)
 
     }
 }
